3a728b08e0fc8364a62975670a13ec810350fcbc
dansim6935 <dansim6935@gmail.com>
2023-11-04 16:26:06 -0700

    Added content to all pages

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,5 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header"
+
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage"
+import PortfolioPage from "./pages/PortfolioPage"
+import ContactPage from "./pages/ContactPage";
+
 import Footer from "./components/Footer"
 
 function App() {
@@ -7,6 +13,12 @@ function App() {
     <div className='app-jsx'>
       <Header/>
       <Outlet/>
+
+      <HomePage/>
+      <AboutPage/>
+      <PortfolioPage/>
+      <ContactPage/>
+      
       <Footer/>
     </div>
   );
---
+++ my-project/src/pages/PortfolioPage.jsx
@@ -0,0 +1,160 @@
+function PortfolioPage() {
+    return (
+        <section class="bg-white dark:bg-gray-900 antialiased">
+            <div class="max-w-screen-xl px-4 py-8 mx-auto lg:px-6 sm:py-16 lg:py-24">
+                <div class="max-w-2xl mx-auto text-center">
+                    <h2 class="text-3xl font-extrabold leading-tight tracking-tight text-gray-900 sm:text-4xl dark:text-white">
+                        Our work
+                    </h2>
+                    <p class="mt-4 text-base font-normal text-gray-500 sm:text-xl dark:text-gray-400">
+                        Crafted with skill and care to help our clients grow their business!
+                    </p>
+                </div>
+
+                <div class="grid grid-cols-1 mt-12 text-center sm:mt-16 gap-x-20 gap-y-12 sm:grid-cols-2 lg:grid-cols-3">
+                    <div class="space-y-4">
+                        <span
+                            class="bg-gray-100 text-gray-900 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">
+                            Alphabet Inc.
+                        </span>
+                        <h3 class="text-2xl font-bold leading-tight text-gray-900 dark:text-white">
+                            Official website
+                        </h3>
+                        <p class="text-lg font-normal text-gray-500 dark:text-gray-400">
+                            Flowbite helps you connect with friends, family and communities of people who share your interests.
+                        </p>
+                        <a href="#" title=""
+                            class="text-gray-500 bg-primary-700 justify-center hover:bg-primary-800 inline-flex items-center  focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                            role="button">
+                            View deployed site
+                        </a>
+                        <a href="#" title=""
+                            class="text-gray-500 bg-primary-700 justify-center hover:bg-primary-800 inline-flex items-center  focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                            role="button">
+                            Visit GitHub
+                        </a>
+                    </div>
+
+                    <div class="space-y-4">
+                        <span
+                            class="bg-gray-100 text-gray-900 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">
+                            Microsoft Corp.
+                        </span>
+                        <h3 class="text-2xl font-bold leading-tight text-gray-900 dark:text-white">
+                            Management system
+                        </h3>
+                        <p class="text-lg font-normal text-gray-500 dark:text-gray-400">
+                            Flowbite helps you connect with friends, family and communities of people who share your interests.
+                        </p>
+                        <a href="#" title=""
+                            class="text-gray-500 bg-primary-700 justify-center hover:bg-primary-800 inline-flex items-center  focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                            role="button">
+                            View deployed site
+                        </a>
+                        <a href="#" title=""
+                            class="text-gray-500 bg-primary-700 justify-center hover:bg-primary-800 inline-flex items-center  focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                            role="button">
+                            Visit GitHub
+                        </a>
+                    </div>
+
+                    <div class="space-y-4">
+                        <span
+                            class="bg-gray-100 text-gray-900 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">
+                            Adobe Inc.
+                        </span>
+                        <h3 class="text-2xl font-bold leading-tight text-gray-900 dark:text-white">
+                            Logo design
+                        </h3>
+                        <p class="text-lg font-normal text-gray-500 dark:text-gray-400">
+                            Flowbite helps you connect with friends, family and communities of people who share your interests.
+                        </p>
+                        <a href="#" title=""
+                            class="text-gray-500 bg-primary-700 justify-center hover:bg-primary-800 inline-flex items-center  focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                            role="button">
+                            View deployed site
+                        </a>
+                        <a href="#" title=""
+                            class="text-gray-500 bg-primary-700 justify-center hover:bg-primary-800 inline-flex items-center  focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                            role="button">
+                            Visit GitHub
+                        </a>
+                    </div>
+                </div>
+
+                <div class="grid grid-cols-1 mt-12 text-center sm:mt-16 gap-x-20 gap-y-12 sm:grid-cols-2 lg:grid-cols-3">
+                    <div class="space-y-4">
+                        <span
+                            class="bg-gray-100 text-gray-900 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">
+                            Alphabet Inc.
+                        </span>
+                        <h3 class="text-2xl font-bold leading-tight text-gray-900 dark:text-white">
+                            Official website
+                        </h3>
+                        <p class="text-lg font-normal text-gray-500 dark:text-gray-400">
+                            Flowbite helps you connect with friends, family and communities of people who share your interests.
+                        </p>
+                        <a href="#" title=""
+                            class="text-gray-500 bg-primary-700 justify-center hover:bg-primary-800 inline-flex items-center  focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                            role="button">
+                            View deployed site
+                        </a>
+                        <a href="#" title=""
+                            class="text-gray-500 bg-primary-700 justify-center hover:bg-primary-800 inline-flex items-center  focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                            role="button">
+                            Visit GitHub
+                        </a>
+                    </div>
+
+                    <div class="space-y-4">
+                        <span
+                            class="bg-gray-100 text-gray-900 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">
+                            Microsoft Corp.
+                        </span>
+                        <h3 class="text-2xl font-bold leading-tight text-gray-900 dark:text-white">
+                            Management system
+                        </h3>
+                        <p class="text-lg font-normal text-gray-500 dark:text-gray-400">
+                            Flowbite helps you connect with friends, family and communities of people who share your interests.
+                        </p>
+                        <a href="#" title=""
+                            class="text-gray-500 bg-primary-700 justify-center hover:bg-primary-800 inline-flex items-center  focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                            role="button">
+                            View deployed site
+                        </a>
+                        <a href="#" title=""
+                            class="text-gray-500 bg-primary-700 justify-center hover:bg-primary-800 inline-flex items-center  focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                            role="button">
+                            Visit GitHub
+                        </a>
+                    </div>
+
+                    <div class="space-y-4">
+                        <span
+                            class="bg-gray-100 text-gray-900 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">
+                            Adobe Inc.
+                        </span>
+                        <h3 class="text-2xl font-bold leading-tight text-gray-900 dark:text-white">
+                            Logo design
+                        </h3>
+                        <p class="text-lg font-normal text-gray-500 dark:text-gray-400">
+                            Flowbite helps you connect with friends, family and communities of people who share your interests.
+                        </p>
+                        <a href="#" title=""
+                            class="text-gray-500 bg-primary-700 justify-center hover:bg-primary-800 inline-flex items-center  focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                            role="button">
+                            View deployed site
+                        </a>
+                        <a href="#" title=""
+                            class="text-gray-500 bg-primary-700 justify-center hover:bg-primary-800 inline-flex items-center  focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                            role="button">
+                            Visit GitHub
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+};
+
+export default PortfolioPage;
